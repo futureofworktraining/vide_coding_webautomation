3becc554599aae58353bc5a5fb48f0b2daffd246
futureofworktraining <80478036+futureofworktraining@users.noreply.github.com>
2025-07-09 13:26:07 +0200

    Update video_recorder.js

--- video_recorder.js
+++ video_recorder.js
@@ -1,22 +1,38 @@
 import { PuppeteerScreenRecorder } from 'puppeteer-screen-recorder';
 
-export const startRecording = async (page) => {
-    const recorder = new PuppeteerScreenRecorder(page, {
+export const startRecording = async (page, quality = 'medium', framerate = 10) => {
+    let videoConfig = {
         followNewPages: true,
-        fps: 10,
+        fps: framerate,
         videoFrame: {
             width: 1920,
             height: 1080,
         },
-        videoCrf: 30,
         videoCodec: 'libx264',
         videoPreset: 'ultrafast',
-        videoBitrate: 1000,
         autopad: {
             color: 'black'
         },
         aspectRatio: '16:9'
-    });
+    };
+
+    switch (quality) {
+        case 'low':
+            videoConfig.videoCrf = 40;
+            videoConfig.videoBitrate = 500;
+            break;
+        case 'high':
+            videoConfig.videoCrf = 20;
+            videoConfig.videoBitrate = 2000;
+            break;
+        case 'medium':
+        default:
+            videoConfig.videoCrf = 30;
+            videoConfig.videoBitrate = 1000;
+            break;
+    }
+
+    const recorder = new PuppeteerScreenRecorder(page, videoConfig);
     await recorder.start('./screenshots/recording.mp4');
     return recorder;
 };
