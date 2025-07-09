f85e6d8bbcc241da1c04c4a48dc0b25bb2481434
futureofworktraining <80478036+futureofworktraining@users.noreply.github.com>
2025-07-09 14:45:40 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -8,7 +8,7 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/futureofworktraining/v
 # Download utility scripts
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/futureofworktraining/vide_coding_webautomation/with-images-and-video/get_cleared_HTML_code.js" -OutFile "./utils/get_cleared_HTML_code.js"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/futureofworktraining/vide_coding_webautomation/with-images-and-video/video_recorder.js" -OutFile "./utils/video_recorder.js"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/futureofworktraining/vide_coding_webautomation/with-images-and-video/dialog_handler.js" -OutFile "./utils/dialog_handler.jss"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/futureofworktraining/vide_coding_webautomation/with-images-and-video/dialog_handler.js" -OutFile "./utils/dialog_handler.js"
 
 # Install dependencies
 Write-Host "Installing dependencies..."
